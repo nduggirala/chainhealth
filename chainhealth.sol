7ac5d72aa893502b2dd7550eb52d2f3839eed91c
Nikhil Duggirala <nikhild@berkeley.edu>
2018-05-14 02:36:47 -0700

    updated code and fixed bugs

--- chainhealth.sol
+++ chainhealth.sol
@@ -9,8 +9,9 @@ contract Record {
     string blood_type;
     string name;
     uint age;
+    string gender;
     
-    function Record(uint _id, string _data, uint _ssn, string _blood_type, string _name, uint _age) public {
+    function Record(uint _id, string _data, uint _ssn, string _blood_type, string _name, uint _age, string _gender) public {
         owner = msg.sender;
         doctor = Doctor(msg.sender);
         id = _id;
@@ -19,30 +20,35 @@ contract Record {
         blood_type = _blood_type;
         name = _name;
         age = _age;
+        gender = _gender;
     }
     
     function getData() public view returns(string) {
-        return data;
+        if (msg.sender == owner) return data;
+    }
+
+    function getGender() public view returns(string) {
+        if (msg.sender == owner) return gender;
     }
 
     function getId() public view returns(uint) {
-        return id;
+        if (msg.sender == owner) return id;
     }
 
     function getSsn() public view returns(uint) {
-        return ssn;
+        if (msg.sender == owner) return ssn;
     }
 
     function getAge() public view returns(uint) {
-        return age;
+        if (msg.sender == owner) return age;
     }
 
     function getBloodType() public view returns(string) {
-        return blood_type;
+        if (msg.sender == owner) return blood_type;
     }
 
     function getName() public view returns(string) {
-        return name;
+        if (msg.sender == owner) return name;
     }
 
     function transfer(address newOwner) public {
@@ -50,32 +56,31 @@ contract Record {
         if (msg.sender != owner) return;
         if (doctor.isTokenTransferOK(owner, newOwner))
             owner = newOwner;
+            doctor = Doctor(newOwner);
+            doctor.addRecord();
+    }
+
+    function getDoctorId() public view returns(int) {
+        return doctor.getDoctorId();
     }
 }
 
 contract Doctor {
-    uint public id;
-    Hospital hospital;
+    int public id;
     address owner;
     Record[] public records;
     
-    function Doctor(uint _id) {
+    function Doctor(int _id) {
         id = _id;
         owner = msg.sender;
-        hospital = Hospital(msg.sender);
     }
     
-    function addRecord(uint _rid, string _data, uint _ssn, string _blood_type, string _name, uint _age) {
-        records.push(new Record(_rid, _data, _ssn, _blood_type, _name, _age));
+    function addRecord(uint _rid, string _data, uint _ssn, string _blood_type, string _name, uint _age, string _gender) {
+        records.push(new Record(_rid, _data, _ssn, _blood_type, _name, _age, _gender));
     }
-    
-    function contains(uint _rid) returns (bool found) {
-        for(uint i=0; i < records.length; i++){
-            if(records[i].id == _rid){
-                return true;
-            }
-        }
-        return true;
+
+    function addRecord() {
+        records.push(Record(msg.sender));
     }
     
     function isTokenTransferOK(address currentOwner, address newOwner)
@@ -85,6 +90,71 @@ contract Doctor {
     {
         address tokenAddress = msg.sender;
         return (keccak256(newOwner) & 0xff) == (bytes20(tokenAddress) & 0xff);
+    }
+
+    function transfer(uint _rid, address newOwner) public {
+        // Only the current owner can transfer the token.
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                records[i].transfer(newOwner);
+                if(records[i].getDoctorId() != id) {
+                    delete records[i];
+                }
+                return;
+            }
+        }
+    }
+
+    function getDoctorId() public view returns (int) {
+        return id;
+    }
+
+    function getData(uint _rid) public view returns(string) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getData();
+            }
+        }
+    }
+
+    function getSsn(uint _rid) public view returns(uint) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getSsn();
+            }
+        }
+    }
+
+    function getAge(uint _rid) public view returns(uint) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getAge();
+            }
+        }
+    }
+
+    function getBloodType(uint _rid) public view returns(string) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getBloodType();
+            }
+        }
+    }
+
+    function getGender(uint _rid) public view returns(string) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getGender();
+            }
+        }
+    }
+
+    function getName(uint _rid) public view returns(string) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].getId() == _rid){
+                return records[i].getName();
+            }
+        }
     }
     
 }
