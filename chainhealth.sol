befa96e03b8930318da7c8de35fc56ec7e88517c
Nikhil Duggirala <nikhild@berkeley.edu>
2018-05-13 19:04:26 -0700

    restructured contracts

--- chainhealth.sol
+++ chainhealth.sol
@@ -1,28 +1,90 @@
 pragma solidity ^0.4.0;
-import "browser/Hospital.sol";
-import "browser/Doctor.sol";
 
-contract System {
+contract Record {
+    string data;
+    uint id;
     address owner;
-    Hospital[] public hospitals;
+    Doctor doctor;
+    uint ssn;
+    string blood_type;
+    string name;
+    uint age;
     
-    function System() {
+    function Record(uint _id, string _data, uint _ssn, string _blood_type, string _name, uint _age) public {
         owner = msg.sender;
+        doctor = Doctor(msg.sender);
+        id = _id;
+        data = _data;
+        ssn = _ssn;
+        blood_type = _blood_type;
+        name = _name;
+        age = _age;
     }
     
-    function addRecord(string _data, uint _rid, Doctor _doctor) {
-        _doctor.addRecord(_data, _rid);
+    function getData() public view returns(string) {
+        return data;
+    }
+
+    function getId() public view returns(uint) {
+        return id;
+    }
+
+    function getSsn() public view returns(uint) {
+        return ssn;
+    }
+
+    function getAge() public view returns(uint) {
+        return age;
+    }
+
+    function getBloodType() public view returns(string) {
+        return blood_type;
+    }
+
+    function getName() public view returns(string) {
+        return name;
+    }
+
+    function transfer(address newOwner) public {
+        // Only the current owner can transfer the token.
+        if (msg.sender != owner) return;
+        if (doctor.isTokenTransferOK(owner, newOwner))
+            owner = newOwner;
+    }
+}
+
+contract Doctor {
+    uint public id;
+    Hospital hospital;
+    address owner;
+    Record[] public records;
+    
+    function Doctor(uint _id) {
+        id = _id;
+        owner = msg.sender;
+        hospital = Hospital(msg.sender);
     }
     
-    function addDoctor(uint _did, Hospital _hospital) {
-        _hospital.addDoctor(_did);
+    function addRecord(uint _rid, string _data, uint _ssn, string _blood_type, string _name, uint _age) {
+        records.push(new Record(_rid, _data, _ssn, _blood_type, _name, _age));
     }
     
-    function addHospital(uint _hid) {
-        hospitals.push(new Hospital(_hid));
+    function contains(uint _rid) returns (bool found) {
+        for(uint i=0; i < records.length; i++){
+            if(records[i].id == _rid){
+                return true;
+            }
+        }
+        return true;
     }
     
-    function transferRecord(uint _rid) {
-        
+    function isTokenTransferOK(address currentOwner, address newOwner)
+        public
+        view
+        returns (bool ok)
+    {
+        address tokenAddress = msg.sender;
+        return (keccak256(newOwner) & 0xff) == (bytes20(tokenAddress) & 0xff);
     }
+    
 }
